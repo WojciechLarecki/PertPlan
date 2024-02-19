8dc96de801011e9fa932d2427df857a8561c00ae
Wojciech <s205838@sggw.edu.pl>
2024-02-19 18:21:28 +0100

    Add csv validation.

--- PertPlan.WebUI/Controllers/TasksController.cs
+++ PertPlan.WebUI/Controllers/TasksController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PertPlan.WebUI.Logics;
+using PertPlan.WebUI.Models;
 using PertPlan.WebUI.Models.ViewModels;
 
 namespace PertPlan.WebUI.Controllers
@@ -26,6 +27,99 @@ namespace PertPlan.WebUI.Controllers
 
             // Data is invalid, return empty view
             return View(projectTasks);
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile csvFile)
+        {
+            var projectTasks = new List<ProjectTask>();
+
+            if (csvFile == null || csvFile.Length == 0)
+            {
+                // ModelState.AddModelError("csvFile", "Nie wybrano pliku lub plik jest pusty.");
+                TempData["ErrorMessage"] = "Nie wybrano pliku lub plik jest pusty.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            using (var reader = new StreamReader(csvFile.OpenReadStream()))
+            {
+                string? line;
+                _ = reader.ReadLine(); //headers line
+                var lineIndex = 1;
+                try
+                {
+                    do
+                    {
+                        line = reader.ReadLine();
+                        lineIndex++;
+
+                        if (line is null) break;
+
+                        var properties = line.Split(';');
+
+                        int taskNumber;
+                        double taskPositiveFinishTime;
+                        double taskAverageFinishTime;
+                        double taskNegativeFinishTime;
+
+                        if (properties.Length != 6)
+                        {
+                            throw new Exception($"Line {lineIndex}: File has wrong structure.");
+                        }
+                        else if (!int.TryParse(properties[0], out taskNumber))
+                        {
+                            throw new Exception($"Line {lineIndex}: Task number is not a number.");
+                        }
+                        else if (!double.TryParse(properties[2], out taskPositiveFinishTime))
+                        {
+                            throw new Exception($"Line {lineIndex}: Positive time is not a number.");
+                        }
+                        else if (!double.TryParse(properties[3], out taskAverageFinishTime))
+                        {
+                            throw new Exception($"Line {lineIndex}: Average time is not a number.");
+                        }
+                        else if (!double.TryParse(properties[4], out taskNegativeFinishTime))
+                        {
+                            throw new Exception($"Line {lineIndex}: Negative time is not a number.");
+                        }
+
+                        try
+                        {
+                            Helper.ValidateTaskName(properties[1]);
+                            Helper.ValidateTaskAverageTime(taskPositiveFinishTime, taskAverageFinishTime, taskNegativeFinishTime);
+                            Helper.ValidateNegativeTimeInput(taskAverageFinishTime, taskNegativeFinishTime);
+                        }
+                        catch(ArgumentException e)
+                        {
+                            throw new Exception($"Line {lineIndex}: {e.Message}");
+                        }
+
+                        var task = new ProjectTask
+                        {
+                            Name = properties[1],
+                            PositiveFinishTime = taskPositiveFinishTime,
+                            AverageFinishTime = taskAverageFinishTime,
+                            NegativeFinishTime = taskNegativeFinishTime
+                        };
+
+                        if (!string.IsNullOrEmpty(properties[5]))
+                        {
+                            Helper.ValidateDependentTasksInput(properties[5], taskNumber);
+                            task.DependOnTasks = properties[5];
+                        }
+
+                        projectTasks.Add(task);
+                    } while (line != null);
+                }
+                catch (Exception e)
+                {
+                    TempData["ErrorMessage"] = e.Message;
+                    return RedirectToAction("Index", "Home");
+                }
+                
+            }
+
+            return View("Index", projectTasks);
         }
     }
 }
---
+++ PertPlan.WebUI/Models/ValidationHelper.cs
@@ -0,0 +1,59 @@
+﻿namespace PertPlan.WebUI.Models
+{
+    public static class Helper
+    {
+        public static void ValidateTaskName(string name)
+        {
+            string trimmedName = name.Trim();
+            if (string.IsNullOrEmpty(trimmedName))
+            {
+                throw new ArgumentException("Name is empty or has only spaces in it.");
+            }
+            if (trimmedName.Length > 100)
+            {
+                throw new ArgumentException("Name is too long. Max size is 100 characters.");
+            }
+        }
+
+        public static void ValidateTaskAverageTime(double positiveTime, double averageTime, double negativeTime)
+        {
+            if (averageTime < positiveTime)
+            {
+                throw new ArgumentException("Średni czas wykonania zadania nie może być krótszy niż pozytywny.");
+            }
+            else if (averageTime > negativeTime)
+            {
+                throw new ArgumentException("Średni czas wykonania zadania nie może być dłuższy niż negatywny.");
+            }
+        }
+
+        public static void ValidateNegativeTimeInput(double averageTime, double negativeTime)
+        {
+            if (negativeTime < averageTime)
+            {
+                throw new ArgumentException("Negatywny czas wykonania zadania nie może być krótszy niż średni.");
+            }
+        }
+
+        public static void ValidateDependentTasksInput(string dependentTasks, int taskNumber)
+        {
+            var tasksNumbers = dependentTasks.Split(',');
+
+            foreach (var numberStr in tasksNumbers)
+            {
+                if (!int.TryParse(numberStr.Trim(), out var number))
+                {
+                    throw new ArgumentException("Ciąg zadań ma niepoprawną strukturę.");
+                }
+                else if (number < 0)
+                {
+                    throw new ArgumentException("Zadania nie mają ujemnych numerów.");
+                }
+                else if (number > taskNumber)
+                {
+                    throw new ArgumentException("Zadanie nie może polegać na jeszcze niezdefiniowanym zadaniu.");
+                }
+            }
+        }
+    }
+}
