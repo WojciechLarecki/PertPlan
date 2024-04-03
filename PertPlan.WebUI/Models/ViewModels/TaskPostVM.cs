aede86c76d2761b3ab6a8b600cf67f3b8d2416e3
Wojciech <s205838@sggw.edu.pl>
2024-04-03 13:50:46 +0200

    Fix the same bug for PDM notation.

--- PertPlan.WebUI/Models/ViewModels/TaskPostVM.cs
+++ PertPlan.WebUI/Models/ViewModels/TaskPostVM.cs
@@ -150,7 +150,7 @@ namespace PertPlan.WebUI.Models.ViewModels
                 }
                 else if (node.Value.NextNodes == null)
                 {
-                    output += chars[node.Value.Id] + $"\n";
+                    output += chars[node.Value.Id] + $"[{node.Value.ToPDMNotation()}]\n";
                     continue;
                 }
 
